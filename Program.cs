7876adfcc130d62d9011a2b17739cbe40d11b125
xtremeelement <xtremeelement@gmail.com>
2020-03-03 11:02:53 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CatWorx.BadgeMaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> employees = new List<string>();
+
+
+
+            while (true)
+            {
+                Console.WriteLine("Please enter a name:\n");
+                string input = Console.ReadLine();
+
+                if (input == "")
+                {
+                    break;
+                }
+
+                employees.Add(input);
+            }
+
+
+
+            foreach (string employee in employees)
+            {
+                Console.WriteLine(employee);
+            }
+
+
+        }
+    }
+}
