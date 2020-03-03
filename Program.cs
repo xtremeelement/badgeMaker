72a6fd8b4df5f03eb7a6443cafdc559eb9bbe0c5
xtremeelement <xtremeelement@gmail.com>
2020-03-03 11:50:12 -0500

    Methods are now returning instances of Employees instead of strings

--- Program.cs
+++ Program.cs
@@ -7,13 +7,13 @@ namespace CatWorx.BadgeMaker
     {
 
         //Function to add employees to the list;
-        static List<string> GetEmployees()
+        static List<Employee> GetEmployees()
         {
-            List<string> employees = new List<string>();
+            List<Employee> employees = new List<Employee>();
 
             while (true)
             {
-                Console.WriteLine("Please enter a name or leave blank to exit:\n");
+                Console.WriteLine("Please enter a name or leave blank to exit:");
                 string input = Console.ReadLine();
 
                 if (input == "")
@@ -24,26 +24,31 @@ namespace CatWorx.BadgeMaker
                 //Instantiate new employee from Employee.cs to create a new Employee instance
                 Employee currentEmployee = new Employee(input, "Smith");
                 //Adds the employees name by using the GetName method from Employee.cs using the parameters passed in above
-                employees.Add(currentEmployee.GetName());
+                employees.Add(currentEmployee);
             }
 
             return employees;
         }
 
         //Function to iterate through the employee list and prints to console
-        static void PrintEmployees(List<string> employees)
+        static void PrintEmployees(List<Employee> employees)
         {
-            foreach (string employee in employees)
+            foreach (Employee employee in employees)
             {
-                Console.WriteLine(employee);
+                Console.WriteLine(employee.GetName());
             }
+
+            // for (int i = 0; i < employees.Count; i++)
+            // {         
+            //     Console.WriteLine(employees[i].GetName());
+            // }
         }
 
         //Entry point to application
         static void Main(string[] args)
         {
             //Calls the GetEmployees function
-            List<string> employees = GetEmployees();
+            List<Employee> employees = GetEmployees();
 
             //Calls the PrintEmployees function passing the employees as a parameter
             PrintEmployees(employees);
