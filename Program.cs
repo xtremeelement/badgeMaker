fa0912ad6b6b0b4c94b6ff142da3385a6ea8457c
xtremeelement <xtremeelement@gmail.com>
2020-03-03 11:15:17 -0500

    Refactored code into seperate methods

--- Program.cs
+++ Program.cs
@@ -5,11 +5,20 @@ namespace CatWorx.BadgeMaker
 {
     class Program
     {
+        //Entry point to application
         static void Main(string[] args)
         {
+            //Calls the GetEmployees function
+            List<string> employees = GetEmployees();
+
+            //Calls the PrintEmployees function passing the employees as a parameter
+            PrintEmployees(employees);
+        }
+
+        //Function to add employees to the list;
+        static List<string> GetEmployees()
+        {
             List<string> employees = new List<string>();
-
-
 
             while (true)
             {
@@ -24,14 +33,16 @@ namespace CatWorx.BadgeMaker
                 employees.Add(input);
             }
 
+            return employees;
+        }
 
-
+        //Function to iterate through the employee list and prints to console
+        static void PrintEmployees(List<string> employees)
+        {
             foreach (string employee in employees)
             {
                 Console.WriteLine(employee);
             }
-
-
         }
     }
 }
